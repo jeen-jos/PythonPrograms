e1edff4ec76128c400523049ab93d502aa972abe
JJ <63134209+jeen-jos@users.noreply.github.com>
2020-10-04 00:11:25 +1000

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,12 @@ print(" The value is ",num*num)
 
 print("the value of 3+4 is ",3+4)
 print("5+6 is ",5+6," and 4+7 is ",4+7)
+
+#Optional Arguments of print()
+#-------------------------------------
+#sep - python insets space between arguments of print()
+print("the value of 3+4 is ",3+4,".",sep=' ')
+
+#end - keeps python print() from advancing automatically to next line
+print("hello friends",end=' ')
+print("Have a great day")
