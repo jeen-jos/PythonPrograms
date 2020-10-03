d7fef7567e09c643fa7c01ec70b6aba198cd73f1
JJ <63134209+jeen-jos@users.noreply.github.com>
2020-10-03 16:22:17 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
+# Follwoing code shows how to print
 msg = "Hello world"
 print(msg)
 print(msg.capitalize())
 print(msg.split())
+
+# Taking inputs from user
+name = input("enter your name : ")
+print("Hello ",name)
+
+# eval() converts entered text into number to evaluate expressions
+num= eval(input("Enter the number : "))
+print(" The value is ",num*num)
+
+print("the value of 3+4 is ",3+4)
+print("5+6 is ",5+6," and 4+7 is ",4+7)
